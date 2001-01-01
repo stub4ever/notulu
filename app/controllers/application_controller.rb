c66d990bc42246ad58868fe51886f8f672c4d0e5
“stub4ever” <bui.vquoc@gmail.com>
2001-01-01 02:12:04 +0100

    Updated style & added dashboard style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+
+  # including the sessions helper module into Application controller
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create #Create action for login
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password]) # Output only by valid user & right password return
+        log_in user #Log the user in and redirect to the user's show page
+        redirect_to user  #Converts this to the route for the user's profile page:
+    else
+      flash.now[:danger] = 'Invalid email/password combination' #An error message
+      render 'new' #retry login
+    end
+  end
 end
