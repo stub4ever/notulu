2b7699bd788d3b0375342321ff0d82516c77d443
“stub4ever” <bui.vquoc@gmail.com>
2015-10-30 14:20:10 +0100

    Added Sign-up Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id]) # retrieve the user id > allow to request to the Users controller > params[:id] will be user id 1 < works the same as User.find(1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'signup' => 'users#new'
+  resources :users # add working /users/1 URL + work together with all the action needed for a s RESTful Users resource with large umber of named routes
+
 end
