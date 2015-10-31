8dcfd3b610b3f79767953e8fba09384b0db452d2
“stub4ever” <bui.vquoc@gmail.com>
2015-10-31 11:15:20 +0100

    Added Error Messages + debug ENV output & renderend content of sidebar & background

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) # retrieve the user id > allow to request to the Users controller > params[:id] will be user id 1 < works the same as User.find(1)
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the sample App!"    #  Adding a flash message to user sign-up
+      redirect_to @user                                 # Allow auto redirect to user_url (@user) > action with a save a redirect
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
