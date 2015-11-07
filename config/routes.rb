8e7ba4769a5d414ec4b5dd30da02e6c842069c68
“stub4ever” <bui.vquoc@gmail.com>
2015-11-07 15:35:07 +0100

    Style form & update title pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
+#get 'sessions/new'
+
   root 'static_pages#home'
 
-  get 'signup' => 'users#new'
-  resources :users # add working /users/1 URL + work together with all the action needed for a s RESTful Users resource with large umber of named routes
+  get 'signup'      => 'users#new'
+
+  get 'login'       => 'sessions#new' #Page for a new sessions(login)
+  post 'login'      => 'sessions#create' #Create a new session(login)
+  delete 'logout'   => 'sessions#destroy' #Delete a session (log out)
+
+  resources :users # add working /users/1 URL + work together with all the action needed for a s RESTful Users resource with large number of named routes
 
 end
