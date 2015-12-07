961ce75b82c46b9af086b1fccba373bd60f9ddba
“stub4ever” <bui.vquoc@gmail.com>
2015-12-07 18:07:50 +0100

    Change admin is fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "quoc",
+             email: "bui.vquoc@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456",
              admin: true)
 
 99.times do |n|
