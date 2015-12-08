0ab818410c1167ee0c0c718c986ced88a8dcc009
“stub4ever” <bui.vquoc@gmail.com>
2015-12-08 19:28:33 +0100

    Style write page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,27 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to current_user
+    else
+      render 'users/create'
+    end
+  end
+
+  def destroy
+  end
+
+  def write
+    @micropost = current_user.microposts.build if logged_in?
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   post 'login'      => 'sessions#create' #Create a new session(login)
   delete 'logout'   => 'sessions#destroy' #Delete a session (log out)
 
+  get 'write'       => 'microposts#write'
+
   resources :users # add working /users/1 URL + work together with all the action needed for a s RESTful Users resource with large number of named routes
   resources :account_activations, only: [:edit] #Resource for account activations
+  resources :microposts,          only: [:create, :destroy] #Allow to create a new post and delete
 end
